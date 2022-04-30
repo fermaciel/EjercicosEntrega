1f9f630adf6ca5c54d36a017338abedd211144f8
fermaciel <104040518+fermaciel@users.noreply.github.com>
2022-04-29 21:10:51 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+//Declaracion de variables
+let vuelta1: number = 10;
+let vuelta2: number = 12;
+let vuelta3: number = 15;
+let vuelta4: number = 13;
+let promedio: number = (vuelta1 + vuelta2 + vuelta3 + vuelta4) / 4;
+console.log("El promedio de  las vueltas es " + promedio);
